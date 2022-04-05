4f1cb0ef952dbcfc8280627cbc2d56e899537dfc
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-04-04 23:00:02 -0300

    Criado requisição da api

--- src/services/tmdb_api.js
+++ src/services/tmdb_api.js
@@ -1,52 +1,54 @@
 const API_KEY = '0767c52392ea03272a3c9d425ea2d929'; //LEMBRAR DE REMOVER ELA //Chave da API (v3 auth) the movie db
 const API_BASE = 'https://api.themoviedb.org/3';
 
-const basicFetch = async (endpoint) => {
-
+const basicFetch = async (endpoint /*endpoint é o final da url json*/) => { //Essa constantante é que vai fazer a requisição na api
+    const req = await fetch(`${API_BASE}${endpoint}`); //Fazendo a requição
+    const json = req.json(); //Convertendo a resposta da requisição em json
+    return json;
 }
 
 export default {
-    getHomeList: async () => { //Retorna vários objetos json que serão usados para criar as listas de filmes
-        return [
+    getHomeList: async () => { //getHomeList é uma função que Retorna vários objetos json que serão usados para criar as listas de filmes
+        return [ //Retorna lista de objetos json dos filmes e series
             {
                 slug: 'originals',
                 title: 'Originais do Netflix',
-                items: []
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}` /*Esse parâmetro que está sendo passado dentro de basicFetch é o endpoint*/)
             },
             {
                 slug: 'trending',
                 title: 'Recomendados para Você',
-                items: []
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}` /*endpoint*/)
             },
             {
                 slug: 'toprated',
                 title: 'Em Alta',
-                items: []
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'action',
                 title: 'Ação',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'comedy',
                 title: 'Comédia',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'horror',
                 title: 'Terror',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'documentary',
                 title: 'Documentários',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
             },
         ];
     }
--- src/views/App.js
+++ src/views/App.js
@@ -1,8 +1,23 @@
+import React, {useEffect} from 'react';
 import './App.css';
 
+//API
+import tmdb_api from '../services/tmdb_api';
+
 export default () => {
+
+  useEffect(() => {
+    const loadAll = async () => { //Função que retorna as informaçẽs da api
+      //Pegando a lista total
+      let list = await tmdb_api.getHomeList();
+      console.log(list);
+    }
+
+    loadAll(); //Executando função para carregar informaçẽs da api
+  }, [])
+
   return (
-    <div className="App">
+    <div>
       <h1>Olá Mundo!</h1>
     </div>
   );
