9e3cb59d96aef11f9487b421f2cc1518dd253e7d
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-04-04 23:37:29 -0300

    updating

---
+++ src/components/movieRow/MovieRow.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './styles.css'
+
+export default () => {
+    return(
+        <div>
+            <h1>Lista de filme</h1>
+        </div>
+    )
+}
--- src/views/App.js
+++ src/views/App.js
@@ -1,24 +1,37 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+
+//Componentes
+import MovieRow from '../components/movieRow/MovieRow';
 
 //API
 import tmdb_api from '../services/tmdb_api';
 
-export default () => {
+export default () => { //Função principal //Nesse caso ela é uma função anônima
+
+  const [movieList, setMovieList] = useState([]); //Guarda o estado de lista de todos os filmes retornados da api
 
   useEffect(() => {
     const loadAll = async () => { //Função que retorna as informaçẽs da api
       //Pegando a lista total
-      let list = await tmdb_api.getHomeList();
-      console.log(list);
+      let list = await tmdb_api.getHomeList(); //Fazendo requição na api
+      setMovieList(list);
     }
 
     loadAll(); //Executando função para carregar informaçẽs da api
   }, [])
 
   return (
-    <div>
-      <h1>Olá Mundo!</h1>
+    <div className='page'>
+      <section className='lists'>
+        {
+          movieList.map((item, key) => (
+            <MovieRow key={key}/>
+          ))
+        
+        }
+      </section>
+      
     </div>
   );
 }
