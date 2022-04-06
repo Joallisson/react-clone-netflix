897e5c36c8a83a87ea23e5fa633c97a5dae18d3b
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-04-05 23:11:01 -0300

    Criando interface e listando filmes e series

--- src/components/movieRow/MovieRow.js
+++ src/components/movieRow/MovieRow.js
@@ -1,10 +1,21 @@
 import React from "react";
 import styles from './styles.css'
 
-export default () => {
+export default ({title, items}) => {
     return(
-        <div>
-            <h1>Lista de filme</h1>
+        <div className="movieRow">
+            <h2>{title}</h2>
+            <div className="movieRow--listArea">
+                <div className="movieRow--list">
+                    { //Se a lista for maior que 0 ou seja se existir um filme na lista, então mostra os filmes da lista
+                    items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className="movieRow--item">
+                            <img alt={item.original_title} src={`https://image.tmdb.org/t/p/w300${item.poster_path}` /*Exibindo posters dos filmes da netflix*/}/>
+                        </div>
+                    ))
+                    }
+                </div>                
+            </div>
         </div>
     )
 }
--- src/views/App.js
+++ src/views/App.js
@@ -3,30 +3,44 @@ import './App.css';
 
 //Componentes
 import MovieRow from '../components/movieRow/MovieRow';
+import FeatureMovie from '../components/featureMovie/FeatureMovie';
 
 //API
 import tmdb_api from '../services/tmdb_api';
 
 export default () => { //Função principal //Nesse caso ela é uma função anônima
 
+  const [featureData, setFeatureData] = useState(null); //Filme em destaque no começo da tela
   const [movieList, setMovieList] = useState([]); //Guarda o estado de lista de todos os filmes retornados da api
 
   useEffect(() => {
     const loadAll = async () => { //Função que retorna as informaçẽs da api
       //Pegando a lista total
-      let list = await tmdb_api.getHomeList(); //Fazendo requição na api
+      let list = await tmdb_api.getHomeList(); //Criando variaǘel para armazenar os filmes //Fazendo requição na api
       setMovieList(list);
+
+      //Pegando o filme em destaque/feature
+      let originals = list.filter(filmes => filmes.slug === 'originals'); //Pegando os filmes originais da netflix
+      let randomChose = Math.floor(Math.random() * (originals[0].items.results.length - 1)); //Gerando número aleatório entre o numero de filmes da netflix e guardando na variável
+      let chosen = originals[0].items.results[randomChose]; //Guardando o filme dentro da variável
+      console.log(chosen)
     }
 
+    
     loadAll(); //Executando função para carregar informaçẽs da api
   }, [])
 
   return (
     <div className='page'>
+
+      {
+        featureData && <FeatureMovie item={featureData}/>
+      }
+
       <section className='lists'>
         {
-          movieList.map((item, key) => (
-            <MovieRow key={key}/>
+          movieList.map((item, key) => ( //Lista de filmes
+            <MovieRow key={key} title={item.title} items={item.items}/>
           ))
         
         }
