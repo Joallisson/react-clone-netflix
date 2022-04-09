65fcaf151b0b92ce859305c7a11d1d272a89c25e
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-04-08 23:03:45 -0300

    Feito o ícone de loading

--- src/views/App.js
+++ src/views/App.js
@@ -73,6 +73,12 @@ export default () => { //Função principal //Nesse caso ela é uma função an�
         Direitos de imagem para Netflix <br/>
         Dados pegos do site Themoviedb.org
       </footer>
+
+      { movieList.length <= 0 &&
+        <div className='loading'>
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt='Loading da Netflix'/>
+        </div>
+      }
       
     </div>
   );
